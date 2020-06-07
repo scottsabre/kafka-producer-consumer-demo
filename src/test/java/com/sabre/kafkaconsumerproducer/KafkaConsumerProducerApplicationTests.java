30c75be4c2ee4c2f31ca1d19026c2a6b2f6c8dc4
Scott Sabre <d.scott.sabre@gmail.com>
2020-06-07 13:26:38 -0500

    Initialzr base project commit

---
+++ src/test/java/com/sabre/kafkaconsumerproducer/KafkaConsumerProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabre.kafkaconsumerproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
